b346640836b456a447bbb6b9aa1a92aa7aa3006d
Grayson <salmonjarl87100@gmail.com>
2024-12-05 21:41:23 -0500

    New test line.

--- arScanner.lua
+++ arScanner.lua
@@ -3,4 +3,6 @@
 -- change this if needed to the directory that contains the main program
 local fileLocation = "/ArScanner"
 
+print("From computer.")
+
 shell.run(fileLocation .. "/main.lua", fileLocation)
