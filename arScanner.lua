80a60a90cab9ee572ce0a51981b2ea79d48b077e
Grayson <salmonjarl87100@gmail.com>
2024-12-05 21:53:15 -0500

    Removed test lines.

--- arScanner.lua
+++ arScanner.lua
@@ -1,8 +1,4 @@
--- from computer
-
 -- change this if needed to the directory that contains the main program
 local fileLocation = "/ArScanner"
-
-print("From computer.")
 
 shell.run(fileLocation .. "/main.lua", fileLocation)
